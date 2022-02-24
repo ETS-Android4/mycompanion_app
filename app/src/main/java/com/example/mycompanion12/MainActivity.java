dede16569e822c7483f1ccb7114abdc08c263b60
Gopika Dinesan <gopikalakshmidinesh@gmail.com>
2022-02-24 09:43:09 +0530

    commit

--- app/src/main/java/com/example/mycompanion12/MainActivity.java
+++ app/src/main/java/com/example/mycompanion12/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.mycompanion12;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
-
+DrawerLayout drawerLayout;
+NavigationView navigationView;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button loginBtn = (Button)findViewById(R.id.button2);
 
+        drawerLayout=findViewById(androidx.activity.ktx.R.id.drawerLayout);
+        navigationView=findViewById(R.id.navigation_view);
+        toolbar=findViewById(R.id.toolbar);
+
+
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
+
+drawerLayout.addDrawerListener(toggle);
+toggle.syncState();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,11 +42,15 @@ public class MainActivity extends AppCompatActivity {
         });
         Button signupBtn = (Button)findViewById(R.id.button3);
 
+
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, signup.class));
             }
         });
+    }
+
+    private void setSupportActionBar(Button signupBtn) {
     }
 }
