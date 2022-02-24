201ee85a25facca2d6f755d143e21ab307ffeb6f
GopikaJaideep <gopikajaideep201199@gmail.com>
2022-02-24 13:46:42 +0530

    SIGN UP COLOUR CHANGE

--- app/src/main/java/com/example/mycompanion12/MainActivity.java
+++ app/src/main/java/com/example/mycompanion12/MainActivity.java
@@ -8,32 +8,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
-
-DrawerLayout drawerLayout;
-NavigationView navigationView;
-Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button loginBtn = (Button)findViewById(R.id.button2);
-
-        drawerLayout=findViewById(androidx.activity.ktx.R.id.drawerLayout);
-        navigationView=findViewById(R.id.navigation_view);
-        toolbar=findViewById(R.id.toolbar);
-
-
-
-        setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
-
-drawerLayout.addDrawerListener(toggle);
-toggle.syncState();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,16 +26,11 @@ toggle.syncState();
             }
         });
         Button signupBtn = (Button)findViewById(R.id.button3);
-
-
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, signup.class));
             }
         });
-    }
-
-    private void setSupportActionBar(Button signupBtn) {
     }
 }
